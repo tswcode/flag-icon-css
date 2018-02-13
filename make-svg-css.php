d82a5cd710eb66f895f39dc65e17dc456bf3ff84
Tom-Steve Watzke <ts.watzke@pitcom.de>
2018-02-13 12:45:51 +0100

    optimizing php script

--- make-svg-css.php
+++ make-svg-css.php
@@ -1,28 +1,9 @@
 #!/usr/bin/php
-.flag-icon-background {
-    background-size: contain;
-    background-position: 50%;
-    background-repeat: no-repeat;
-}
-.flag-icon {
-    background-size: contain;
-    background-position: 50%;
-    background-repeat: no-repeat;
-    position: relative;
-    display: inline-block;
-    width: 1.33333333em;
-    line-height: 1em;
-}
-.flag-icon:before {
-    content: "\00a0";
-}
-.flag-icon.flag-icon-squared {
-    width: 1em;
-}
 <?php
 
 const PATH_DEFAULT = __DIR__ . '/flags/4x3';
 const PATH_SQUARE  = __DIR__ . '/flags/1x1';
+const INCLUDE_SQUARE = false;
 const FLAG_LIST    = [
         'wm' => [
             'ar', 'au',
@@ -42,6 +23,33 @@ const FLAG_LIST    = [
         ]
 ];
 
+?>
+.flag-icon-background {
+    background-size: contain;
+    background-position: 50%;
+    background-repeat: no-repeat;
+}
+.flag-icon {
+    background-size: contain;
+    background-position: 50%;
+    background-repeat: no-repeat;
+    position: relative;
+    display: inline-block;
+    width: 1.33333333em;
+    line-height: 1em;
+}
+.flag-icon:before {
+    content: "\00a0";
+}
+<?php if(INCLUDE_SQUARE): ?>
+.flag-icon.flag-icon-squared {
+    width: 1em;
+}
+<?php endif ?>
+.flag-sprite {
+    width: 20px;
+}
+<?php
 $dirListDefault = scandir(PATH_DEFAULT);
 
 foreach($dirListDefault as $entry) {
@@ -55,21 +63,23 @@ foreach($dirListDefault as $entry) {
     $flagFileContent       = file_get_contents($flagFilePath);
     $flagFileContent       = str_replace("\n", '', str_replace('"', "'", $flagFileContent));
     $flagFileContent       = preg_replace('/\>\s*\</', '><', $flagFileContent);
-    $flagFileContent       = base64_encode($flagFileContent);
 
-    $flagFileSquarePath    = PATH_SQUARE  . "/$entry";
-    $flagFileSquareContent = file_get_contents($flagFileSquarePath);
-    $flagFileSquareContent = str_replace("\n", '', str_replace('"', "'", $flagFileSquareContent));
-    $flagFileSquareContent = preg_replace('/\>\s*\</', '><', $flagFileSquareContent);
-    $flagFileSquareContent = base64_encode($flagFileSquareContent);
+    if(INCLUDE_SQUARE) {
+        $flagFileSquarePath = PATH_SQUARE . "/$entry";
+        $flagFileSquareContent = file_get_contents($flagFileSquarePath);
+        $flagFileSquareContent = str_replace("\n", '', str_replace('"', "'", $flagFileSquareContent));
+        $flagFileSquareContent = preg_replace('/\>\s*\</', '><', $flagFileSquareContent);
+    }
 
 ?>
 .flag-icon-<?= $flagName ?> {
     background-image: url("data:image/svg+xml;utf8,<?= $flagFileContent ?>");
 }
+<?php if(INCLUDE_SQUARE): ?>
 .flag-icon-<?= $flagName ?>.flag-icon-squared {
     background-image: url("data:image/svg+xml;utf8,<?= $flagFileSquareContent ?>");
 }
+<?php endif ?>
 <?php
 }
 
